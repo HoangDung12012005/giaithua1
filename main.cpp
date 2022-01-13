c08b35b184b6342710bd91478421f67bb1e41d24
HoangDung12012005 <97678049+HoangDung12012005@users.noreply.github.com>
2022-01-13 21:13:34 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+# giaithua1
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	
+	int n;
+		cin >> n;
+		{
+     int S = 1;
+	for ( int i = 1; i <= n; i++)
+	
+		S = S * i;
+		cout << S;
+		}
+		
+	
+	return 0;
+	
+
+}
+© 2022 GitHub, Inc.
+Terms
